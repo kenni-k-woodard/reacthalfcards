e99e8a860c89696d329b56ba4f1c3f9305f2dbe4
Kenni Woodard <kennikristine@gmail.com>
2023-01-25 16:31:40 -0800

    setFrom has been refactored

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import CardList from './CardList';
 
-export default function Player({
-  player,
-  setTo,
-  hand,
-  setFrom,
-  // setSelectedCard,
-  // selectedCard,
-  to,
-}) {
+export default function Player({ player, setTo, hand, to }) {
   return (
     <div
       className={`player ${to === player ? 'selected-player' : ''}`}
       onClick={() => setTo(player)}
     >
       <p>Player {player}</p>
-      <CardList player={player} cards={hand} setFrom={setFrom} />
+      <CardList player={player} cards={hand} />
     </div>
   );
 }
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,9 +3,11 @@ import { createContext, useContext, useState } from 'react';
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState();
+  // const [playerOneHand, setPlayerOneHand] = useState();
+  const [from, setFrom] = useState('deck');
 
   return (
-    <GameContext.Provider value={{ selectedCard, setSelectedCard }}>
+    <GameContext.Provider value={{ selectedCard, setSelectedCard, from, setFrom }}>
       {children}
     </GameContext.Provider>
   );
