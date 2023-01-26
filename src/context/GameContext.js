9fa0bfa2cc84342e36630fd74022515f1c976064
Kenni Woodard <kennikristine@gmail.com>
2023-01-25 17:30:27 -0800

    refactored the setTo situation

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,11 +3,11 @@ import { createContext, useContext, useState } from 'react';
 const GameContext = createContext();
 const GameProvider = ({ children }) => {
   const [selectedCard, setSelectedCard] = useState();
-  // const [playerOneHand, setPlayerOneHand] = useState();
   const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
 
   return (
-    <GameContext.Provider value={{ selectedCard, setSelectedCard, from, setFrom }}>
+    <GameContext.Provider value={{ selectedCard, setSelectedCard, from, setFrom, to, setTo }}>
       {children}
     </GameContext.Provider>
   );
